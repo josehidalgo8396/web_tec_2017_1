193c0e1b4c68fe1df8f29ab69eb0569bece59338
josehidalgo8396 <josehidalgo8396@gmail.com>
2017-04-22 14:01:59 -0600

    Parte #3

--- Tarea Investigacion3/main.py
+++ Tarea Investigacion3/main.py
@@ -1,7 +1,12 @@
 import requests
 import cssutils
+import psycopg2
+import time
 from bs4 import BeautifulSoup
 
+db = psycopg2.connect(database="d70j23m846rhsg", user="qprrzlbbnjntfc", password="2bc9e656799c9b685fbcf2ab4f3c170b3e03d21ce840e41358bbcff21461655c", host="ec2-50-19-95-47.compute-1.amazonaws.com", port="5432")
+global registros
+registros = 0
 def getData(link):
     lista = []
     page = requests.get(link)
@@ -14,18 +19,15 @@ def getData(link):
         if len(image) > 200:
             image = "no image"
         lista.append(image)
-        #print(image)
 
         second = soup.find("div", attrs={"id":"tekst1"})
         title = second.select("h1")
         if title[0].get_text() != "Tajo alto, Miramar 00":
-            lista.append(title[0].get_text()) #caracteristicas internas
-            #print(title[0].get_text())
+            lista.append(title[0].get_text()) 
 
             fourth = soup.find("p", attrs={"id":"text2"})
             price = fourth.get_text()
             lista.append(price)
-            #print(price)
 
             mainArticle = soup.find("section", attrs={"class":"mainarticles"})
 
@@ -35,39 +37,33 @@ def getData(link):
             for i in characteristics:
                 if i.select("label")[0].get_text() != "Precio" and i.select("label")[0].get_text() != "precio" and i.select("label")[0].get_text() != "Caracteristicas internas" and i.select("label")[0].get_text() != "Caracteristicas Internas":
                     lista.append(i.select("p")[0].get_text())
-                    #print(i.select("p")[0].get_text())
 
             seenDiv = mainArticle.find("div", attrs={"class":"left"})
             seen = seenDiv.select("p")[0].get_text()
             seen = seen.split(" ")
             lista.append(seen[0])
             lista.append(seen[4] + " " + seen[5] + " " + seen[6])
-            #print(seen)
-            #print(lista)
+
             if len(lista) == 16:
-                print("Imagen: " + lista[0])
-                print("Titulo: " + lista[1])
-                print("Precio: " + lista[2])
-                print("Tipo: " + lista[3])
-                print("Compra o Alquiler: " + lista[4])
-                print("Caracteristicas externas: " + lista[5])
-                print("Superficie: " + lista[6])
-                print("Tamaño: " + lista[7])
-                print("Camas: " + lista[8])
-                print("Baños: " + lista[9])
-                print("Mascotas: " + lista[10])
-                print("Fecha de Construccion: " + lista[11])
-                print("Ubicacion: " + lista[12])
-                print("Tipo de Edificio: " + lista[13])
-                print("Visto: " + lista[14] + " veces")
-                print("Ultima fecha de visto: " + lista[15])
-                print("\n\n")
+                insertData(lista)
+
+def insertData(lista):
+    global registros
+    query = ("insert into Registros(imagen, titulo, precio, tipo, venta, externas, superficie, tamano, camas, banos, mascotas, fechaConstruccion, ubicacion, tipoEdificio, visto, fechaVisto) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+    lista[8] = lista[8].replace("+","")
+    lista[9] = lista[9].replace("+","")
+    houseData = tuple(lista)
+    cursor = db.cursor()
+    cursor.execute(query,houseData)
+    db.commit()
+    cursor.close()
+    registros+=1
 
 def main(link):
+    global registros
     cont = 1
     while cont < 22:
         print("Pagina: " + str(cont))
-        print("------------------------------------------------------------------------------------------------")
         if cont == 1:
             page = requests.get(link)
             soup = BeautifulSoup(page.content, "html.parser")
@@ -82,6 +78,10 @@ def main(link):
             for i in houses:
                 getData(link+i["href"][9:])
         cont+=1
+    cursor = db.cursor()
+    cursor.execute("insert into Auditoria(fecha, pagina_web, numero_registros, estado, errores) values(%s,%s,%s,%s,%s)",(time.strftime("%d-%m-%Y"), "https://www.casas.cr/c/casas/", registros, "finalizado", "no errors"))
+    db.commit()
+    cursor.close()
     print("Proceso terminado")
 
 main("https://www.casas.cr/c/casas/")
